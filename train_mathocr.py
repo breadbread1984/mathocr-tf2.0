ce02c84a3c3a8955248fbb709bc7bf2baf5c8069
breadbread1984 <breadbread1984@163.com>
2019-06-01 10:47:54 +0800

    add preprocess

--- train_mathocr.py
+++ train_mathocr.py
@@ -3,7 +3,7 @@
 import tensorflow as tf;
 from Model import Encoder, Decoder;
 
-def parse_function(serialized_example):
+def parse_function(serialized_example, crop = True, transform = True):
     context, sequence = tf.io.parse_single_sequence_example(
         serialized_example,
         context_features = {
@@ -16,6 +16,7 @@ def parse_function(serialized_example):
             'tokens': tf.io.FixedLenSequenceFeature((), dtype = tf.int64)
         }
     );
+    # parse
     data = tf.io.decode_raw(context['data'], out_type = tf.uint8);
     data = tf.reshape(data, (256,256,1));
     data = tf.cast(data, dtype = tf.float32);
@@ -25,6 +26,17 @@ def parse_function(serialized_example):
     tokens = sequence['tokens'];
     tf.Assert(tf.equal(tf.shape(text)[0],text_length),[tf.shape(text)]);
     tf.Assert(tf.equal(tf.shape(tokens)[0],tokens_length),[tf.shape(tokens)]);
+    
+    #preprocess
+    if crop:
+        pos = tf.where(tf.greater(255. - tf.squeeze(data),0.));
+        min_yx = tf.reduce_min(pos, axis = 0);
+        max_yx = tf.reduce_max(pos, axis = 0);
+        hw = max_yx - min_yx;
+        data = tf.image.crop_to_bounding_box(data, min_yx[0], min_yx[1], hw[0], hw[1]);
+    if transform:
+        data = tf.image.resize(data, (128,128))
+        
     return data, tokens, text;
 
 def main():
@@ -34,7 +46,10 @@ def main():
     testset = tf.data.TFRecordDataset('testset.tfrecord').map(parse_function);
     while True:
         for data, tokens, _ in trainset:
-            # TODO
+            import cv2;
+            img = data.numpy().astype('uint8');
+            cv2.imshow('img',img);
+            cv2.waitKey();
 
 if __name__ == "__main__":
 
