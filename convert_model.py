611f7074954e40da215c00108d401ac9e212f0a7
breadbread1984 <breadbread1984@163.com>
2019-06-05 10:56:25 +0800

    add convert model

---
+++ convert_model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import tensorflow as tf;
+
+def main():
+
+    tf.keras.backend.set_learning_phase(0);
+    encoder = tf.keras.models.load_model('encoder.h5');
+    decoder = tf.keras.models.load_model('decoder.h5');
+    tf.saved_model.save(encoder, './encoder/1');
+    tf.saved_model.save(decoder, './decoder/1');
+    encoder_loaded = tf.saved_model.load('./encoder/1');
+    decoder_loaded = tf.saved_model.load('./decoder/1');
+    encoder_infer = encoder_loaded.signatures['serving_default'];
+    decoder_infer = decoder_loaded.signatures['serving_default'];
+    print('====================NOTE==================');
+    print('encoder\'s output tensor name is', encoder_infer.structured_outputs);
+    print('decoder\'s output tensor name is', decoder_infer.structured_outputs);
+
+if __name__ == "__main__":
+
+    assert tf.executing_eagerly();
+    main();
+
