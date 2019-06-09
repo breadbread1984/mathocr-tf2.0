5859e1a7c1f15b4a4fbf4f90d1ec8017bd4dc224
breadbread1984 <breadbread1984@163.com>
2019-06-09 09:36:37 +0800

    update model

--- Model.py
+++ Model.py
@@ -4,61 +4,65 @@ import pickle;
 import numpy as np;
 import tensorflow as tf;
 
-def Bottleneck(code_shape, growth_rate, dropout_rate = 0.2):
-    
-    # dimension of output is the same as input's
-    code = tf.keras.Input(shape = code_shape);
-    results = tf.keras.layers.BatchNormalization()(code);
-    results = tf.keras.layers.ReLU()(results);
-    results = tf.keras.layers.Conv2D(filters = 3 * growth_rate, kernel_size = (1,1), use_bias = False)(results);
-    results = tf.keras.layers.BatchNormalization()(results);
-    results = tf.keras.layers.ReLU()(results);
-    results = tf.keras.layers.Conv2D(filters = growth_rate, kernel_size = (3,3), padding = 'same', use_bias = False)(results);
-    results = tf.keras.layers.Dropout(rate = dropout_rate)(results);
-    results = tf.keras.layers.Concatenate()([code, results]);
-    return tf.keras.Model(code = code, outputs = results);
-
-def Transition(code_shape, output_filters):
-    
-    # h,w of output = 1/2 h, 1/2 w of input's
-    code = tf.keras.Input(shape = code_shape);
-    results = tf.keras.layers.BatchNormalization()(code);
-    results = tf.keras.layers.ReLU()(results);
-    results = tf.keras.layers.Conv2D(filters = output_filters, kernel_size = (1,1), use_bias = False)(results);
-    results = tf.keras.layers.AveragePooling2D(pool_size = (2,2), strides = (2,2))(results);
-    return tf.keras.Model(code = code, outputs = results);
-
-def BottleneckArray(code_shape, growth_rate, depth, dropout_rate = 0.2):
-    
-    # dimension of output is the same as input's
-    code = tf.keras.Input(shape = code_shape);
-    results = code;
-    for i in range(depth):
-        results = Bottleneck(results.shape[1:], growth_rate, dropout_rate)(results);
-    return tf.keras.Model(code = code, outputs = results);
-
-def Encoder(code_shape, output_filters = 48, dropout_rate = 0.2):
+def DenseNet(input_shape, blocks = 3, level = 16, growth_rate = 24, output_filters = 48, dropout_rate = 0.2):
     
     # h,w of outA(low_res) = 1/16 h, 1/16 w of input's
     # h,w of outB(high_res) = 1/8 h, 1/8 w of input's
-    code = tf.keras.Input(shape = code_shape);
-    results = tf.keras.layers.Conv2D(filters = output_filters, kernel_size = (7,7), strides = (2,2), padding = 'same', use_bias = False)(code);
-    results = tf.keras.layers.BatchNormalization()(results);
+    inputs = tf.keras.Input(shape = input_shape);
+    results = tf.keras.layers.Conv2D(filters = output_filters, kernel_size = (7,7), strides = (2,2), padding = 'same', use_bias = False)(inputs);
+    results = tf.keras.layers.BatchNormalization(momentum = 0.9, gamma_initializer = 'glorot_uniform', epsilon = 0.0001)(results);
     results = tf.keras.layers.ReLU()(results);
-    # height and width are halved
+    # NOTE: height and width are halved
     results = tf.keras.layers.MaxPool2D(pool_size = (2,2), strides = (2,2))(results);
-    results = BottleneckArray(results.shape[1:], growth_rate = 24, depth = 16, dropout_rate = dropout_rate)(results);
-    # height and width are halved
-    results = Transition(results.shape[1:], results.shape[-1] // 2)(results);
-    results = BottleneckArray(results.shape[1:], growth_rate = 24, depth = 16, dropout_rate = dropout_rate)(results);
-    results = tf.keras.layers.BatchNormalization()(results);
-    before_trans2 = tf.keras.layers.ReLU()(results);
-    results = tf.keras.layers.Conv2D(filters = before_trans2.shape[-1] // 2, kernel_size = (1,1), use_bias = False)(before_trans2);
-    # height and width are halved
-    results = tf.keras.layers.AveragePooling2D(pool_size = (2,2), strides = (2,2))(results);
-    outA = BottleneckArray(results.shape[1:], growth_rate = 24, depth = 16, dropout_rate = dropout_rate)(results);
-    outB = BottleneckArray(before_trans2.shape[1:], growth_rate = 24, depth = 8, dropout_rate = dropout_rate)(before_trans2);
-    return tf.keras.Model(code = code, outputs = (outA, outB));
+    for i in range(blocks):
+        # denseblock
+        for j in range(level):
+            shortcut = results;
+            # conv 1x1
+            results = tf.keras.layers.Conv2D(filters = 4 * growth_rate, kernel_size = (1,1), use_bias = False)(results);
+            results = tf.keras.layers.BatchNormalization(momentum = 0.9, gamma_initializer = 'glorot_uniform', epsilon = 0.0001)(results);
+            results = tf.keras.layers.ReLU()(results);
+            results = tf.keras.layers.Dropout(rate = dropout_rate)(results);
+            # conv 3x3
+            results = tf.keras.layers.Conv2D(filters = growth_rate, kernel_size = (3,3), padding = 'same', use_bias = False)(results);
+            results = tf.keras.layers.BatchNormalization(momentum = 0.9, gamma_initializer = 'glorot_uniform', epsilon = 0.0001)(results);
+            results = tf.keras.layers.ReLU()(results);
+            results = tf.keras.layers.Dropout(rate = dropout_rate)(results);
+            # bottleneck out
+            results = tf.keras.layers.Concatenate()([shortcut, results]);
+        # transition
+        if i < blocks - 1:
+            results = tf.keras.layers.Conv2D(filters = results.shape[-1] // 2, kernel_size = (1,1), use_bias = False)(results);
+            results = tf.keras.layers.BatchNormalization(momentum = 0.9, gamma_initializer = 'glorot_uniform', epsilon = 0.0001)(results);
+            results = tf.keras.layers.ReLU()(results);
+            results = tf.keras.layers.Dropout(rate = dropout_rate)(results);
+            if i == blocks - 2: before_last_denseblock = results;
+            # NOTE: height and width are halved
+            results = tf.keras.layers.AveragePooling2D(pool_size = (2,2), strides = (2,2))(results);
+
+    return tf.keras.Model(inputs = inputs, outputs = (results, before_last_denseblock));
+
+def Encoder(input_shape, blocks = 3, level = 16, growth_rate = 24, output_filters = 48, dropout_rate = 0.2):
+    
+    inputs = tf.keras.Input(shape = input_shape);
+    outA, results = DenseNet(inputs.shape[1:], blocks, level, growth_rate, output_filters, dropout_rate)(inputs);
+    for i in range(level):
+        shortcut = results;
+        # conv 1x1
+        results = tf.keras.layers.Conv2D(filters = 4 * growth_rate, kernel_size = (1,1), use_bias = False)(results);
+        results = tf.keras.layers.BatchNormalization(momentum = 0.9, gamma_initializer = 'glorot_uniform', epsilon = 0.0001)(results);
+        results = tf.keras.layers.ReLU()(results);
+        results = tf.keras.layers.Dropout(rate = dropout_rate)(results);
+        # conv 3x3
+        results = tf.keras.layers.Conv2D(filters = growth_rate, kernel_size = (3,3), padding = 'same', use_bias = False)(results);
+        results = tf.keras.layers.BatchNormalization(momentum = 0.9, gamma_initializer = 'glorot_uniform', epsilon = 0.0001)(results);
+        results = tf.keras.layers.ReLU()(results);
+        results = tf.keras.layers.Dropout(rate = dropout_rate)(results);
+        # bottleneck out
+        results = tf.keras.layers.Concatenate()([shortcut, results]);
+    outB = results;
+
+    return tf.keras.Model(inputs = inputs, outputs = (outA, outB));
 
 def CoverageAttention(code_shape, s_t_shape, alpha_sum_shape, output_filters, kernel_size):
 
@@ -83,17 +87,17 @@ def CoverageAttention(code_shape, s_t_shape, alpha_sum_shape, output_filters, ke
     alpha_t = tf.keras.layers.Softmax()(tf.keras.layers.Flatten()(e_t));
     # alpha_t.shape = (batch, input_height, input_width, 1)
     alpha_t = tf.keras.layers.Reshape((code.shape[1], code.shape[2], 1,))(alpha_t);
-    new_alpha_sum = tf.keras.layers.Add()([alpha_sum, tf.stop_gradient(alpha_t)]);
+    new_alpha_sum = tf.keras.layers.Add()([alpha_sum, alpha_t]);
     # weighted_inputs.shape = (batch, input_height, input_width, input_filters)
     alpha_t = tf.keras.layers.Lambda(lambda x: tf.tile(x, (1,1,1,code.shape[-1])))(alpha_t);
     weighted_inputs = tf.keras.layers.Multiply()([alpha_t, code]);
     # context.shape = (batch, input_filters)
     context = tf.keras.layers.Lambda(lambda x: tf.math.reduce_sum(x, axis = [1,2]))(weighted_inputs);
-    return tf.keras.Model(code = (code, s_t, alpha_sum), outputs = (context, new_alpha_sum));
+    return tf.keras.Model(inputs = (code, s_t, alpha_sum), outputs = (context, new_alpha_sum));
 
 def Decoder(low_res_shape, high_res_shape, hidden_shape, attn_sum_low_shape, attn_sum_high_shape, num_classes, embedding_dim = 256, hidden_size = 256):
         
-    code = tf.keras.Input(shape = (1,)); # previous token
+    prev_token = tf.keras.Input(shape = (1,)); # previous token
     low_res = tf.keras.Input(shape = low_res_shape); # image low resolution encode
     high_res = tf.keras.Input(shape = high_res_shape); # image high resolution encode
     # context
@@ -101,18 +105,18 @@ def Decoder(low_res_shape, high_res_shape, hidden_shape, attn_sum_low_shape, att
     alpha_sum_low = tf.keras.Input(shape = attn_sum_low_shape);
     alpha_sum_high = tf.keras.Input(shape = attn_sum_high_shape);
         
-    # code.shape = (batch, seq_length = 1)
+    # prev_token.shape = (batch, seq_length = 1)
     # hidden.shape = (batch, hidden size = 256)
-    # embedded.shape = (batch, seq_length = 1,embedding size = 256)
-    embedded = tf.keras.layers.Embedding(input_dim = num_classes, output_dim = embedding_dim)(code);
+    # y_tm1.shape = (batch, seq_length = 1,embedding size = 256)
+    y_tm1 = tf.keras.layers.Embedding(input_dim = num_classes, output_dim = embedding_dim)(prev_token);
     # s_t.shape = (batch, hidden size = 256)
-    s_t = tf.keras.layers.GRU(units = hidden_size)(embedded, initial_state = hidden);
-    # s_t.shape = (batch, 512)
-    s_t = tf.keras.layers.Dense(units = 512, use_bias = False)(s_t);
+    s_t = tf.keras.layers.GRU(units = hidden_size)(y_tm1, initial_state = hidden);
+    # hat_s_t.shape = (batch, 512)
+    hat_s_t = tf.keras.layers.Dense(units = 512, use_bias = False)(s_t);
     # context_low.shape = (batch, 512)
-    context_low, new_attn_sum_low = CoverageAttention(low_res.shape[1:], s_t.shape[1:], alpha_sum_low.shape[1:], output_filters = 256, kernel_size = (11,11))([low_res, s_t, alpha_sum_low]);
+    context_low, new_attn_sum_low = CoverageAttention(low_res.shape[1:], hat_s_t.shape[1:], alpha_sum_low.shape[1:], output_filters = 256, kernel_size = (11,11))([low_res, hat_s_t, alpha_sum_low]);
     # context_high.shape = (batch, 512)
-    context_high, new_attn_sum_high = CoverageAttention(high_res.shape[1:], s_t.shape[1:], alpha_sum_high.shape[1:], output_filters = 256, kernel_size = (7,7))([high_res, s_t, alpha_sum_high]);
+    context_high, new_attn_sum_high = CoverageAttention(high_res.shape[1:], hat_s_t.shape[1:], alpha_sum_high.shape[1:], output_filters = 256, kernel_size = (7,7))([high_res, hat_s_t, alpha_sum_high]);
     # context.shape = (batch, 1024)
     context = tf.keras.layers.Concatenate(axis = -1)([context_low,context_high]);
     # c_t.shape = (batch,seq_length = 1, 1024)
@@ -124,14 +128,14 @@ def Decoder(low_res_shape, high_res_shape, hidden_shape, attn_sum_low_shape, att
     # w_c.shape = (batch, embedding size = 256)
     w_c = tf.keras.layers.Dense(units = embedding_dim, use_bias = False)(context);
     # out.shape = (batch, embedding size = 256)
-    embedded = tf.keras.layers.Reshape((embedded.shape[-1],))(embedded);
-    out = tf.keras.layers.Add()([embedded, w_s, w_c]);
+    y_tm1 = tf.keras.layers.Reshape((y_tm1.shape[-1],))(y_tm1);
+    out = tf.keras.layers.Add()([y_tm1, w_s, w_c]);
     # out.shape = (batch, 128)
     out = tf.keras.layers.Reshape((out.shape[-1] // 2, 2,))(out);
     out = tf.keras.layers.Lambda(lambda x: tf.math.reduce_max(x, axis = -1))(out);
     # out.shape = (batch, num classes)
     out = tf.keras.layers.Dense(units = num_classes, use_bias = False)(out);
-    return tf.keras.Model(code = (code,low_res,high_res,hidden,alpha_sum_low,alpha_sum_high), outputs = (out, tf.stop_gradient(new_hidden), new_attn_sum_low, new_attn_sum_high));
+    return tf.keras.Model(inputs = (prev_token,low_res,high_res,hidden,alpha_sum_low,alpha_sum_high), outputs = (out, new_hidden, new_attn_sum_low, new_attn_sum_high));
 
 class MathOCR(tf.keras.Model):
     
@@ -140,7 +144,7 @@ class MathOCR(tf.keras.Model):
     PAD = "<PAD>";
     SPECIAL_TOKENS = [START, END, PAD];
     
-    def __init__(self, code_shape = (128,128,1,), output_filters = 48, dropout_rate = 0.2, embedding_dim = 256, hidden_size = 256, tokens_length_max = 90):
+    def __init__(self, input_shape = (128,128,1,), output_filters = 48, dropout_rate = 0.2, embedding_dim = 256, hidden_size = 256, tokens_length_max = 90):
         
         super(MathOCR, self).__init__();
         with open('token_id_map.dat','rb') as f:
@@ -150,13 +154,13 @@ class MathOCR(tf.keras.Model):
         
         self.hidden_size = hidden_size;
         self.tokens_length_max = tokens_length_max;
-        self.encoder = Encoder(code_shape[-3:], output_filters, dropout_rate);
+        self.encoder = Encoder(input_shape[-3:], 3, 16, 24, output_filters, dropout_rate);
         self.decoder = Decoder(
-            self.encoder.layers[-2].output_shape[1:],
-            self.encoder.layers[-1].output_shape[1:],
+            self.encoder.outputs[0].shape[1:],
+            self.encoder.outputs[1].shape[1:],
             (hidden_size,),
-            (tf.constant(code_shape[-3:]) // (16, 16, code_shape[-1],)).numpy(),
-            (tf.constant(code_shape[-3:]) // (8, 8, code_shape[-1],)).numpy(),
+            (tf.constant(input_shape[-3:]) // (16, 16, input_shape[-1],)).numpy(),
+            (tf.constant(input_shape[-3:]) // (8, 8, input_shape[-1],)).numpy(),
             len(self.token_to_id),
             embedding_dim,
             hidden_size
@@ -176,9 +180,9 @@ class MathOCR(tf.keras.Model):
         i = tf.constant(0);
         token_id = tf.ones((batch_num,1), dtype = tf.int64) * self.token_to_id[self.START];
         out = tf.zeros((batch_num, len(self.token_to_id)), dtype = tf.float32);
-        hidden = tf.zeros((batch_num, self.hidden_size));
-        alpha_sum_low = tf.zeros(img_shape // (1, 16, 16, img_shape[-1]));
-        alpha_sum_high = tf.zeros(img_shape // (1, 8, 8, img_shape[-1]));
+        hidden = tf.zeros((batch_num, self.hidden_size), dtype = tf.float32);
+        alpha_sum_low = tf.zeros(img_shape // (1, 16, 16, img_shape[-1]), dtype = tf.float32);
+        alpha_sum_high = tf.zeros(img_shape // (1, 8, 8, img_shape[-1]), dtype = tf.float32);
 
         def step(i, prev_token_id, prev_hidden, prev_alpha_sum_low, prev_alpha_sum_high):
             # predict Ua token
@@ -201,11 +205,11 @@ class MathOCR(tf.keras.Model):
         token_id_sequence = tf.concat(token_id_sequence, axis = 1);
 
         # convert to readable string
-        code = token_id_sequence.numpy();
-        code_shape = code.shape;
-        flattened = np.reshape(code,(-1));
+        inputs = token_id_sequence.numpy();
+        inputs_shape = inputs.shape;
+        flattened = np.reshape(inputs,(-1));
         outputs = list(map(lambda x: self.id_to_token[x], flattened));
-        outputs = np.reshape(outputs,(code_shape[0],-1));
+        outputs = np.reshape(outputs,(inputs_shape[0],-1));
         outputs = [''.join(sample) for sample in outputs]; 
         return outputs, logits_sequence;
 
