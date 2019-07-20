f41bb306bff36c1fabb822b982b1e76a8d9cb95f
breadbread1984 <breadbread1984@163.com>
2019-07-20 14:23:01 +0800

    update preprocess

--- Predictor.py
+++ Predictor.py
@@ -10,7 +10,7 @@ class Predictor(object):
     def __init__(self, input_shape = (128,128,3), weights_path = 'models/mathocr_22500.h5'):
 
         self.mathocr = MathOCR(input_shape);
-        self.mathocr.load_weights(weights_path);
+        #self.mathocr.load_weights(weights_path);
 
     def predict(self, img):
 
@@ -31,7 +31,7 @@ class Predictor(object):
         pos = tf.where(tf.greater(255. - tf.squeeze(data),0.));
         min_yx = tf.reduce_min(pos, axis = 0);
         max_yx = tf.reduce_max(pos, axis = 0);
-        center_yx = min_yx + max_yx // 2;
+        center_yx = min_yx + (max_yx - min_yx) // 2;
         lengths = tf.tile(tf.expand_dims(tf.math.reduce_max(max_yx - min_yx),0),(2,));
         min_yx = tf.cast(center_yx - lengths // 2, dtype = tf.int64);
         max_yx = tf.cast(min_yx + lengths, dtype = tf.int64);
