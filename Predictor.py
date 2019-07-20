109568f4c0d33cdb05c97d1980f65973b9e029f3
breadbread1984 <breadbread1984@163.com>
2019-07-20 14:33:03 +0800

    update predictor

--- Predictor.py
+++ Predictor.py
@@ -7,10 +7,10 @@ from Model import MathOCR, convert_to_readable;
 
 class Predictor(object):
 
-    def __init__(self, input_shape = (128,128,3), weights_path = 'models/mathocr_22500.h5'):
+    def __init__(self, input_shape = (128,128,3), weights_path = 'models/mathocr_35800.h5'):
 
         self.mathocr = MathOCR(input_shape);
-        #self.mathocr.load_weights(weights_path);
+        self.mathocr.load_weights(weights_path);
 
     def predict(self, img):
 
